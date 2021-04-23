b0e63bac26740a1d98ac56fd511557d902f24aa3
hallienicholas <57117680+hallienicholas@users.noreply.github.com>
2021-04-23 09:39:25 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
-const hostname = '153.42.144.30';
-const port = process.env.PORT || 80;
+const hostname = '127.0.0.1';
+const port = process.env.PORT || 4000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
