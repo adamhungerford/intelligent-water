aee665f8cc45f834ac0a3959f455fe7521c8fff1
adamhungerford <adhungerford02@gmail.com>
2021-04-24 15:59:36 -0400

    Testing CSS.

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const http = require('http');
 const hostname = '0.0.0.0';
 const port = process.env.PORT || 4000;
 
+document.write("<link rel='stylesheet' type='text/css' href='sb-admin-2.css'>");
+
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
 
