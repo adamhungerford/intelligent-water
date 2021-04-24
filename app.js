811b92ffc74c4b1e086ae7e98d0076144206af50
adamhungerford <adhungerford02@gmail.com>
2021-04-24 16:01:31 -0400

    Reverting changes.

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ const http = require('http');
 const hostname = '0.0.0.0';
 const port = process.env.PORT || 4000;
 
-document.write("<link rel='stylesheet' type='text/css' href='sb-admin-2.css'>");
-
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
 
