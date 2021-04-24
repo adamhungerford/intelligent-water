5c52f769d6f47090fd62280936a1a682526776b1
adamhungerford <adhungerford02@gmail.com>
2021-04-24 15:54:47 -0400

    Trying out a push.

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const port = process.env.PORT || 4000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
+
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World');
 });
